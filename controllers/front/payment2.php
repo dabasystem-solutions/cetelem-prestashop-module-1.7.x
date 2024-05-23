593a17bb9adafd68d3bfcf76678ae6908322a602
Daniel <dgarcia@dabasystem.com>
2024-05-23 13:44:24 +0200

    Change field ZIP code

--- controllers/front/payment2.php
+++ controllers/front/payment2.php
@@ -176,7 +176,7 @@ class CetelemPayment2ModuleFrontController extends ModuleFrontController
                 'birthday' => $birthday,
                 'address' => $addressText,
                 'city' => $address->city,
-                'CodPostalEnvio' => $address->postcode,
+                'CodigoPostalEnvio' => $address->postcode,
                 'email' => $this->context->customer->email,
                 'phone1' => $address->phone,
                 'phone2' => $address->phone_mobile,
